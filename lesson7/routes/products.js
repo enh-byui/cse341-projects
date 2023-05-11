aa27fe8d73eabf1b65c0e34279f51bc701eba2bd
happy-arts <happy.arts.nft@gmail.com>
2023-05-11 02:14:16 -0600

    Updates swagger file

--- lesson7/routes/products.js
+++ lesson7/routes/products.js
@@ -5,23 +5,38 @@ const productsController = require('../controllers/products');
 const validation = require('../middleware/validate');
 
 router.get('/', productsController.getAll
-// #swagger.tags = ['Products']
+/* 
+#swagger.tags = ['Products']
+#swagger.summary = 'Lists all products'
+*/
 );
 
 router.get('/:id', productsController.getSingle
-// #swagger.tags = ['Products']
+/* 
+#swagger.tags = ['Products']
+#swagger.summary = 'Shows one product'
+*/
 );
 
 router.post('/', validation.saveProduct, productsController.createProduct
-// #swagger.tags = ['Products']
+/* 
+#swagger.tags = ['Products']
+#swagger.summary = 'Creates a new product'
+*/
 );
 
 router.put('/:id', validation.saveProduct, productsController.updateProduct
-// #swagger.tags = ['Products']
+/* 
+#swagger.tags = ['Products']
+#swagger.summary = 'Updates one product'
+*/
 );
 
 router.delete('/:id', productsController.deleteProduct
-// #swagger.tags = ['Products']
+/* 
+#swagger.tags = ['Products']
+#swagger.summary = 'Deletes one product'
+*/
 );
 
 module.exports = router;
